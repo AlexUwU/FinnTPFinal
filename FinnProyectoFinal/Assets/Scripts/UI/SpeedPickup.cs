4410e9e2dcdcd0fab324b97eca01549249d8a214
AlexUwUr <gastoncolpari2015@gmail.com>
2025-11-16 20:16:43 -0300

    Ultimos Cambios

--- FinnProyectoFinal/Assets/Scripts/UI/SpeedPickup.cs
+++ FinnProyectoFinal/Assets/Scripts/UI/SpeedPickup.cs
@@ -2,16 +2,23 @@ using UnityEngine;
 
 public class SpeedPickup : MonoBehaviour
 {
-    public float bonusSpeed = 1f;    // cuánto suma SOLO en esta partida
+    public float bonusSpeed = 1f;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            // Sumar velocidad SOLO para esta partida
             GameManager.runSpeedBonus += bonusSpeed;
-            // opcional: efecto, sonido, etc.
+
+            //  Reproducir audio correcto desde el player
+            PlayerBehaviour pb = other.GetComponent<PlayerBehaviour>();
+            if (pb != null && pb.upgradeSound != null)
+                pb.upgradeSound.Play();
+
             Destroy(gameObject);
         }
     }
 }
+
 
